b3227ab5902905d08a9e56ce5001db8955096217
2u4u <tufu@2U4U.com.cn>
2011-04-13 13:33:54 +0800

    play with theme overwrite

---
+++ sites/dhg.2u4u.com.cn/themes/custom/dhg/views-overwrite/views-exposed-form.tpl.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file views-exposed-form.tpl.php
+ *
+ * This template handles the layout of the views exposed filter form.
+ *
+ * Variables available:
+ * - $widgets: An array of exposed form widgets. Each widget contains:
+ * - $widget->label: The visible label to print. May be optional.
+ * - $widget->operator: The operator for the widget. May be optional.
+ * - $widget->widget: The widget itself.
+ * - $sort_by: The select box to sort the view using an exposed form.
+ * - $sort_order: The select box with the ASC, DESC options to define order. May be optional.
+ * - $items_per_page: The select box with the available items per page. May be optional.
+ * - $offset: A textfield to define the offset of the view. May be optional.
+ * - $reset_button: A button to reset the exposed filter applied. May be optional.
+ * - $button: The submit button for the form.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($q)): ?>
+  <?php
+    // This ensures that, if clean URLs are off, the 'q' is added first so that
+    // it shows up first in the URL.
+    print $q;
+  ?>
+<?php endif; ?>
+    <?php foreach($widgets as $id => $widget): ?>
+        <?php if (!empty($widget->label)): ?>
+          <label for="<?php print $widget->id; ?>">
+            <?php print $widget->label; ?>
+          </label>
+        <?php endif; ?>
+        <?php if (!empty($widget->operator)): ?>
+            <?php print $widget->operator; ?>
+        <?php endif; ?>
+          <?php print $widget->widget; ?>
+    <?php endforeach; ?>
+    <?php if (!empty($sort_by)): ?>
+        <?php print $sort_by; ?>
+        <?php print $sort_order; ?>
+    <?php endif; ?>
+    <?php if (!empty($items_per_page)): ?>
+        <?php print $items_per_page; ?>
+    <?php endif; ?>
+    <?php if (!empty($offset)): ?>
+        <?php print $offset; ?>
+    <?php endif; ?>
+      <?php print $button ?>
+    <?php if (!empty($reset_button)): ?>
+        <?php print $reset_button; ?>
+    <?php endif; ?>
