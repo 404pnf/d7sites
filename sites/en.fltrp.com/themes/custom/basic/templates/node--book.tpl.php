9619409ce117d100570242934667dcd291cb4d32
2u4u <tufu@2U4U.com.cn>
2011-07-06 14:53:37 +0800

    en.fltrp 升级views并安装ubercart

--- sites/en.fltrp.com/themes/custom/basic/templates/node--book.tpl.php
+++ sites/en.fltrp.com/themes/custom/basic/templates/node--book.tpl.php
@@ -9,7 +9,7 @@
        <div class="content">
          <div class="english">
           <div class="fenlei_english">English</div>
-          <div class="en_bookpart1">
+          <div class="bookpart1">
               <div class="bookpic">               
                 <?php print render($content['field_img']); ?>
                 <div class="largerpic"><a href="#">See Larger Image</a></div>
@@ -29,7 +29,7 @@
                 </div>
               </div>  
           </div>     
-          <div class="en_bookpart2">
+          <div class="bookpart2">
              <div class="english_content">
                     <div class="english_contents_title">CONTENTS:</div>
                     <?php if (strlen($node->field_directory['und'][0]['value'])>0): ?>
@@ -55,26 +55,88 @@
                     <?php if (strlen($node->field_english_author_info['und'][0]['value'])>0): ?>
                           <?php print $node->field_english_author_info['und'][0]['value'];?>
                     <?php else: ?>
-                          <div class="engliash_none"><?php //print "none"; ?></div>
+                          <div class="engliash_none"><?php print "none"; ?></div>
                     <?php endif; ?>
              </div>       
              <div class="enlish_other_recommendation">
                     <div class="english_other_recommendation_title">OTHER RECOMMENDATION:</div>
-                        <?php           
-                             $output="";
-                             $node_terms=taxonomy_node_get_terms($node , $key = 'tid');  
-                             foreach($node_terms as $tid => $tmp_term){
-                                  $output .= $tid.",";
-                             }
-                             $output=substr($output,0,strlen($output)-1);
-                             $relate_ebook ='';
-                             $relate_ebook =views_embed_view('book_recommendation','block_1',$node->nid,$output);
-                             $relate_ebook = '<div class="ebook_none_mrelate">'.$relate_ebook.'</div>'; 
-                        ?>
+                       <?php          
+                             $output=$field_en_fenlei[0]['tid'];
+                             $relate_book =views_embed_view('book_recommendation','block_1',$output);
+                             $relate_book= '<div class="book_none_mrelate">'.$relate_book.'</div>'; 
+                             print $relate_book;
+			?>
                    
 
           </div> 
          </div>
+	 </div>
+	 
+         <div class="chinese">
+          <div class="fenlei_chinese">中文</div>
+          <div class="bookpart1">
+              <div class="bookpic">               
+                <?php print render($content['field_img']); ?>
+                <div class="largerpic"><a href="#">See Larger Image</a></div>
+              </div>
+              <div class="bookinfo">
+                <strong>图书:<?php print $node->field_title['und'][0]['value']; ?></strong><br>
+                <strong>ISBN: </strong><?php print $node->field_isbn['und'][0]['value'];?><br>
+                <strong>CP No:</strong><?php ?><?php print $node->field_materielno['und'][0]['value'];?><br>
+                <strong>作者:</strong><?php print $node->field_author['und'][0]['value'];?><br>
+                <strong>尺寸:</strong><?php print $node->field_english_dimensions['und'][0]['value'];?><br>
+                <strong>页数:</strong><?php print $node->field_pages['und'][0]['value'];?><br>
+                <strong>重量:</strong><?php print $node->field_weight['und'][0]['value'];?><br>
+                <strong>出版日期:</strong><?php print $node->field_publish_date['und'][0]['value'];?><br>
+                <strong>配套产品:</strong><?php print $node->field_components['und'][0]['value'];?><br>
+                <div class="notice">Not for sale out the territory of the People's Republic of China. Any<br>
+                                    requestof purchase please visit <a href="#">Links</a> 
+                </div>
+              </div>  
+          </div>     
+          <div class="bookpart2">
+             <div class="english_content">
+                    <div class="english_contents_title">目录:</div>
+                    <?php if (strlen($node->field_directory['und'][0]['value'])>0): ?>
+                          <?php print $node->field_directory['und'][0]['value'];?>
+                    <?php else: ?>
+                          <div class="engliash_none"><?php print "none"; ?></div>       
+                    <?php endif; ?>
+             </div>
+             <div class="english_des">
+                    <div class="english_des_title">图书简介：</div>
+                    <?php print $node->body['und'][0]['value'];?>
+             </div>
+             <div class="engliash_preview">
+                    <div class="engliash_preview_title">精彩样张:</div>
+                    <?php if (strlen($node->field_sample['und'][0]['value'])>0): ?>
+                          <?php print $node->field_sample['und'][0]['value'];?>
+                    <?php else: ?>
+                          <div class="engliash_none"><?php print "none"; ?></div>
+                    <?php endif; ?>                  
+             </div>
+             <div class="english_about_author">
+                    <div class="english_about_author_title">关于作者:</div>
+                    <?php if (strlen($node->field_author_info['und'][0]['value'])>0): ?>
+                          <?php print $node->field_author_info['und'][0]['value'];?>
+                    <?php else: ?>
+                          <div class="engliash_none"><?php print "none"; ?></div>
+                    <?php endif; ?>
+             </div>       
+             <div class="enlish_other_recommendation">
+                    <div class="english_other_recommendation_title">推荐图书:</div>
+                       <?php          
+                             $output=$field_en_fenlei[0]['tid'];
+                             $relate_book =views_embed_view('book_recommendation','block_1',$output);
+                             $relate_book= '<div class="book_none_mrelate">'.$relate_book.'</div>'; 
+                             print $relate_book;
+			?>
+                   
+
+          </div> 
+         </div>
+	 </div>
+
        </div>
      
 
