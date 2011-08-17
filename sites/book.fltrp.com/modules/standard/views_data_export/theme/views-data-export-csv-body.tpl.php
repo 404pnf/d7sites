be5c155dd43f2df99651ebcd056a31133762f0d6
fltrp <fltrp@2u4u.com.cn>
2011-08-17 08:43:30 +0800

    老葛修改了 sites/book.fltrp.com/modules/standard/views_data_export/theme/views-data-export.tpl.php 文件。之前的文件，给导出的每个row结尾都加 \r\n 也就是一个windwos的回车（\r），一个linux的换行符（\n） 我们在实际feeds导入发现最后一行如果是空白行，有时候会出问题。 因此老葛修改了该导出模板，判断了一下，如果是最后一行，就不跟\r\n。
    具体最后一行的空白是否会引起问题，我们不缺定。

--- sites/book.fltrp.com/modules/standard/views_data_export/theme/views-data-export-csv-body.tpl.php
+++ sites/book.fltrp.com/modules/standard/views_data_export/theme/views-data-export-csv-body.tpl.php
@@ -1,6 +1,9 @@
 <?php
 
 // Print out exported items.
+$total = count($themed_rows);
+$i =0;
 foreach ($themed_rows as $count => $item_row):
-  print implode($separator, $item_row) . "\r\n";
+  $i++;
+  print implode($separator, $item_row) . (($i != $total) ? "\r\n" : "");
 endforeach;
