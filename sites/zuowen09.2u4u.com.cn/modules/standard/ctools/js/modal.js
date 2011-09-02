ee94f01c158c654483241512723026c9251a8af2
fltrp <fltrp@2u4u.com.cn>
2011-09-02 15:58:29 +0800

    zw09站点升级到d7，把代码先传上来

--- sites/zuowen09.2u4u.com.cn/modules/standard/ctools/js/modal.js
+++ sites/zuowen09.2u4u.com.cn/modules/standard/ctools/js/modal.js
@@ -244,6 +244,10 @@
       $('#modal-content form:not(.ctools-use-modal-processed)', context)
         .addClass('ctools-use-modal-processed')
         .each(function() {
+          $('input[type=submit], button', this).click(function() {
+            this.form.clk = this;
+          });
+
           var element_settings = {};
 
           element_settings.url = $(this).attr('action');
@@ -253,12 +257,6 @@
 
           Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
           Drupal.ajax[base].form = $(this);
-
-          $('input[type=submit], button', this).click(function() {
-            Drupal.ajax[base].element = this;
-            this.form.clk = this;
-          });
-
         });
     }
   };
