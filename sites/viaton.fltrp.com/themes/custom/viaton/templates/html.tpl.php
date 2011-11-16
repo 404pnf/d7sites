ba66058a72e89d4477a1540ed38938741cca8cb4
fltrp <fltrp@2u4u.com.cn>
2011-11-16 10:02:59 +0800

    建设viaton

--- sites/viaton.fltrp.com/themes/custom/viaton/templates/html.tpl.php
+++ sites/viaton.fltrp.com/themes/custom/viaton/templates/html.tpl.php
@@ -9,6 +9,60 @@
 
   <?php print $styles; ?>
 
+<!---   added by zhanglin  //-->
+<?
+	global $base_url;
+	 $current_path   = drupal_get_path_alias();
+
+	 switch($current_path){
+	 case "home":
+          $custom_style_file ='/css/index.css';
+		 break;
+	 case "products":
+          $custom_style_file ='/css/product.css';
+		 break;
+	 case "productslist":
+          $custom_style_file ='/css/product.css';
+		 break;
+	 case "productsdetails":
+          $custom_style_file ='/css/product.css';
+		 break;
+	case "news":
+          $custom_style_file ='/css/information.css';
+		 break;	
+	case "newsmore":
+          $custom_style_file ='/css/information.css';
+	      break;
+	case "famous":
+          $custom_style_file ='/css/salon.css';
+			break;
+	case "newsdetails":
+          $custom_style_file ='/css/information.css';
+	      break;
+	case "searcher":
+          $custom_style_file ='/css/searcher.css';
+		 break;
+	 }
+
+
+ if(stristr($current_path,'newsdetails/'))
+			   $custom_style_file ='/css/information.css';
+
+ if(stristr($current_path,'productslist/')||stristr($current_path,'product/')||stristr($current_path,'products/'))
+			   $custom_style_file ='/css/product.css';
+
+ if(stristr($current_path,'famousdetails/'))
+			   $custom_style_file ='/css/salon.css';
+
+if(!empty($custom_style_file)){
+    $custom_style_uri = ' <style type="text/css" media="all">@import url("'.$base_url.'/'.path_to_theme().$custom_style_file.'");</style>';
+  //  print $base_url.'/'.drupal_get_path('theme','iculture').$custom_style_file;
+	print   $custom_style_uri ;
+}
+
+
+?>
+<!---   added by zhanglin//  -->
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
