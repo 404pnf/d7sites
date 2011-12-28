664a18c78381b24a5cd52f408c1876190e031629
fltrp <fltrp@2u4u.com.cn>
2011-12-28 11:21:00 +0800

    bm.russian第三方模块太老了。升级除了feeds_tamper外所有模块

--- sites/bm.russian.fltrp.com/modules/standard/video/wysiwyg_plugins/video/video.js
+++ sites/bm.russian.fltrp.com/modules/standard/video/wysiwyg_plugins/video/video.js
@@ -3,61 +3,66 @@
   Drupal.wysiwyg.plugins['video'] = {
 
     /**
-   * Return whether the passed node belongs to this plugin.
-   */
+     * Return whether the passed node belongs to this plugin.
+     */
     isNode: function(node) {
       return ($(node).is('img.wysiwyg-video'));
     },
 
     /**
-   * Execute the button.
-   */
-    invoke: function(data, settings, instanceId) {
-      if (data.format == 'html') {
-        // Prevent duplicating
-        if ($(data.node).is('img.wysiwyg-video')) {
-          return;
+     * Execute the button.
+     */
+    invoke: function(data, settings, instanceId) { 
+      Drupal.behaviors.video_wysiwyg.videoBrowser(function(nid, data, settings, instanceId){
+        if (data.format == 'html') {
+          // Prevent duplicating
+          if ($(data.node).is('img.wysiwyg-video')) {
+            return;
+          }
+          var width = 176;
+          var height = 144;
+          var dimensions = Drupal.settings.wysiwyg.plugins.drupal.video.golbal.dimensions;
+          if(dimensions){
+            var wxh = dimensions.split('x');
+            var width = parseInt(wxh[0]);
+            var height = parseInt(wxh[1]);
+          }
+          var content = Drupal.wysiwyg.plugins['video']._getPlaceholder(settings, Drupal.settings.basePath +'video/embed/' +nid+'/'+width+'/'+height, width, height);
         }
-        var content = this._getPlaceholder(settings);
-      }
-      else {
-        // Prevent duplicating.
-        // @todo data.content is the selection only; needs access to complete content.
-        if (data.content.match(/[content:video]/)) {
-          return;
+        else {
+          // Prevent duplicating.
+          // @todo data.content is the selection only; needs access to complete content.
+          if (data.content.match(/[content:video]/)) {
+            return;
+          }
+          var content = nid;
         }
-        var content = '[content:video]';
-      }
-      if (typeof content != 'undefined') {
-        Drupal.wysiwyg.instances[instanceId].insert(content);
-      }
+        if (typeof content != 'undefined') {
+          Drupal.wysiwyg.instances[instanceId].insert(content);
+        }
+      }, data, settings, instanceId);
     },
 
     /**
-   * Replace all [[content:video]] tags with images.
-   */
+     * Replace all [[content:video]] tags with images.
+     */
     attach: function(content, settings, instanceId) {
-      content = content.replace(/\[content:video\]/g, this._getPlaceholder(settings));
       return content;
     },
 
     /**
-   * Replace images with [[content:video]] tags in content upon detaching editor.
-   */
+     * Replace images with [[content:video]] tags in content upon detaching editor.
+     */
     detach: function(content, settings, instanceId) {
-      var $content = $('<div>' + content + '</div>');
-      $.each($('img.wysiwyg-video', $content), function (i, elem) {
-        var tag = ('[content:video]');
-        $(this).replaceWith(tag);
-      });
-      return $content.html();
+      //      return $content.html();
+      return content;
     },
 
     /**
-   * Helper function to return a HTML placeholder.
-   */
-    _getPlaceholder: function (settings) {
-      return '<img src="' + settings.path + '/images/spacer.gif" alt="Video" title="Video" class="wysiwyg-video drupal-content" height="325px" />';
+     * Helper function to return a HTML placeholder.
+     */
+    _getPlaceholder: function (settings, src, width, height) {
+      return '<iframe width="'+width+'" height="'+height+'" src="'+src+'" frameborder="0" allowfullscreen scrolling="no"></iframe>';
     }
   };
 
