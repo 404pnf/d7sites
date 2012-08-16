19e8648f3932e6b86448689a8be27b53943acc65
404 <404@example.com>
2012-08-16 11:24:15 +0800

    del oudated files from @dhg and @en

--- sites/dhg.2u4u.com.cn/themes/custom/dhg/layouts/left/left.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="login_top">                                                                                                                                          
-    <?php print $content['login_top']; ?>                                                                                                                        
-</div>                                                                                                                                                       
-
-<div id="faq_bottom">                                                                                                                                           
-    <?php print $content['faq_bottom']; ?>                                                                                                                         
-</div>                                                                                                                                                       
-
